f236d515a6ed26abd90d8d0fdc42c5b8dce23d9b
OpenShift guest <pavol@starek.sk>
2013-09-21 15:36:57 -0400

    Logging functions by log level
    implemented additional functions for individial log levels

--- sandbox.js
+++ sandbox.js
@@ -2,4 +2,4 @@ var logging = require("./src/logging");
 var l = logging.getLogger("xxx");
 
 l.log(l.INFO, "%s", "ddd", {dfdf:"ddd"});
-
+l.debug("%s", "dd");
--- src/logging.js
+++ src/logging.js
@@ -1,12 +1,16 @@
 var logging = module.exports = {};
 var util = require("util");
 
-logging.INFO = "INFO";
-
-logging.Logger = function(name) {
+logging.Logger = function(name, level) {
     this.name = name || "unknown";
+    this.level = level || this.DEBUG;
 }
+
+logging.Logger.prototype.DEBUG = "TRACE";
+logging.Logger.prototype.DEBUG = "DEBUG";
 logging.Logger.prototype.INFO = "INFO";
+logging.Logger.prototype.INFO = "WARN";
+logging.Logger.prototype.INFO = "ERROR";
 
 logging.Logger.prototype.log = function(level, message) {
     var lev = level;
@@ -14,6 +18,36 @@ logging.Logger.prototype.log = function(level, message) {
     var date = new Date();
     process.stdout.write(date.toLocaleTimeString() + "." + date.getSeconds() + date.getMilliseconds() + " " + lev + " " + this.name + " " + util.format.apply(util, arguments) + "\n");
     
+}
+
+logging.Logger.prototype.trace = function() {
+    var args = Array.prototype.slice.call(arguments, 0);
+    args.unshift(this.TRACE);
+    logging.Logger.prototype.log.apply(this, args);
+}
+
+logging.Logger.prototype.debug = function() {
+    var args = Array.prototype.slice.call(arguments, 0);
+    args.unshift(this.DEBUG);
+    logging.Logger.prototype.log.apply(this, args);
+}
+
+logging.Logger.prototype.info = function() {
+    var args = Array.prototype.slice.call(arguments, 0);
+    args.unshift(this.INFO);
+    logging.Logger.prototype.log.apply(this, args);
+}
+
+logging.Logger.prototype.warn = function() {
+    var args = Array.prototype.slice.call(arguments, 0);
+    args.unshift(this.WARN);
+    logging.Logger.prototype.log.apply(this, args);
+}
+
+logging.Logger.prototype.error = function() {
+    var args = Array.prototype.slice.call(arguments, 0);
+    args.unshift(this.ERROR);
+    logging.Logger.prototype.log.apply(this, args);
 }
 
 logging.getLogger = function(name) {
